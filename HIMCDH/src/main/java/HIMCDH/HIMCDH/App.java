2050cb60ced0495ce1d1d2e0da82e4ff3e0745e2
Aurélien RASTEL <aurelien.rastel@gmail.com>
2014-09-12 14:20:52 +0200

    initial commit after the creation of the maven project

---
+++ HIMCDH/src/main/java/HIMCDH/HIMCDH/App.java
@@ -0,0 +1,13 @@
+package HIMCDH.HIMCDH;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
