d4ce5af3bdf996cdcd0ea8a4fdc9575247c88813
manuclaeys <claeys.esiea@gmail.com>
2014-10-08 16:24:48 +0200

    Code Classe Adress et Contact
    Faut vérifier ce que j'ai fait pr la hashmap et le UUDI je connais pas
    bien. Sinon voila :)

--- Code/WhereTact/wheretact/src/main/java/com/wheretact/models/Address.java
+++ Code/WhereTact/wheretact/src/main/java/com/wheretact/models/Address.java
@@ -1,19 +1,79 @@
 package com.wheretact.models;
 
+
 import java.util.UUID;
 
 public class Address {
 
 	private UUID addressId;
 	private Boolean isBillingAddress = false;
+	private String  postcode, city, state,country;
 	
 	/***
 	 * TO DO : 
-	 * 		-	Attributs Address
-	 * 		-	Constructeur -> génère un randomUUID()!
-	 * 		-	Getters
-	 * 		-	Setters
+	 * 		-	Attributs Address OK
+	 * 		-	Constructeur -> génère un randomUUID()! (ok à verifier)
+	 * 		-	Getters  OK
+	 * 		-	Setters  OK
 	 * 		
 	 * 		-	Equals
+	 * 
+	 * v1 manu 08/10
 	 */
+	
+	public UUID getAddressId(){
+			return addressId;	
+	}
+	
+	public void setAddressId(UUID addressID){
+		this.addressId = addressId; 
+	}
+	
+	
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getPostcode() {
+		return postcode;
+	}
+
+	public void setPostcode(String postcode) {
+		this.postcode = postcode;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+	public static Address builder(UUID addressId, String street, String postCode, String city, String state, String country) {
+		
+		
+		
+		Address newAddress = new Address();
+		newAddress.setAddressId(addressId.randomUUID()); 
+		newAddress.setCity(city);
+		newAddress.setPostcode(postCode);
+		newAddress.setState(state);
+		newAddress.setCountry(country);
+		
+		return newAddress;
+	}
+	
 }
--- Code/WhereTact/wheretact/src/main/java/com/wheretact/models/Contact.java
+++ Code/WhereTact/wheretact/src/main/java/com/wheretact/models/Contact.java
@@ -9,14 +9,111 @@ public class Contact {
 	private Address billingAddress;
 	private HashMap<String, Address> addressList;
 	
+	
+	
 	/***
 	 * TO DO : 
-	 * 		-	Attributs Contact
-	 * 		-	Constructeur -> génère un randomUUID()!
-	 * 		-	Getters
-	 * 		-	Setters
+	 * 		-	Attributs Contact ok
+	 * 		-	Constructeur -> génère un randomUUID()! a verifier
+	 * 		-	Getters ok
+	 * 		-	Setters ok
 	 * 
-	 * 		-	Equals
+	 * 		-	Equals gné??
 	 */
+	
+	private Integer phoneNumbers;
+	private String emails;
+	private String firstname;
+	private String lastname;
+	private String birthdate; 
+	
+	
+	public void setContactId(UUID contactId){
+		this.contactId = contactId; 
+	}
+	
+	public UUID getContactId(){
+		return contactId; 
+	}
+	
+	
+	public Integer getPhoneNumbers() {
+		return phoneNumbers;
+	}
+
+	public void setPhoneNumbers(Integer phones) {
+		this.phoneNumbers = phones;
+	}
+
+	public String getEmails() {
+		return emails;
+	}
+
+	public void setEmails(String emails) {
+		this.emails = emails;
+	}
+
+	public Address getBillingAddress() {
+		return billingAddress;
+	}
+
+	public void setBillingAddress(Address billingAddress) {
+		this.billingAddress = billingAddress; 
+		
+	}
+	
+	
+	public HashMap<String, Address> getAddressList() {
+		return addressList;
+	}
+	
+	public void setAddressList(String key,Address value) { // A VERIFIER
+		addressList.put(key, value);
+	}
+	
+	public String getFirstname() {
+		return firstname;
+	}
+	
+	public void setFirstname(String firstName) {
+		this.firstname = firstName;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String forname) {
+		this.lastname = forname;
+	}
+
+	public String getBirthdate() {
+		return birthdate;
+	}
+
+	public void setBirthdate(String birthdate) {
+		this.birthdate = birthdate;
+	}
+
+	
+	public static Contact builder(UUID contactId,String birthdate,String emails,String firstname, String lastname,Integer phoneNumbers) {
+		
+		Contact newContact = new Contact();
+		
+		newContact.setContactId(contactId.randomUUID()); 
+		newContact.setBirthdate(birthdate);
+		newContact.setEmails(emails);
+		newContact.setFirstname(firstname);
+		newContact.setLastname(lastname);
+		newContact.setPhoneNumbers(phoneNumbers);
+		
+		
+		return newContact;
+	}
+
+	
+	
+	
+	
 	
 }
